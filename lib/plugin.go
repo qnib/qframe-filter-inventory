780d96b3741f85d1b9cd31b38f1facfbee077dbe
Christian Kniep <christian@qnib.org>
2017-05-11 12:46:55 +0200

    specify pluginPkg

--- lib/plugin.go
+++ lib/plugin.go
@@ -15,6 +15,7 @@ import (
 const (
 	version = "0.1.1"
 	pluginTyp = qtypes.FILTER
+	pluginPkg = "inventory"
 )
 
 type Plugin struct {
@@ -24,7 +25,7 @@ type Plugin struct {
 
 func New(qChan qtypes.QChan, cfg config.Config, name string) Plugin {
 	return Plugin{
-		Plugin: qtypes.NewNamedPlugin(qChan, cfg, pluginTyp, name, version),
+		Plugin: qtypes.NewNamedPlugin(qChan, cfg, pluginTyp, pluginPkg, name, version),
 		Inventory: qframe_inventory.NewInventory(),
 	}
 }
